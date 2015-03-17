04566e13aa7a3bfaabaa8a76f69f60da8fca9e49
edborden <borden.edward@gmail.com>
2015-03-17 08:27:58 -0400

    unicorn + auth include

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,4 +1,6 @@
 class MessageController < ActionController::API
+	include ActionController::HttpAuthentication::Basic::ControllerMethods
+
 	http_basic_authenticate_with name: "1faafdceeba45ad26f97a061b", password: "afbc67f939eea5b3f5753dba7c5fe"
 
 	def create
