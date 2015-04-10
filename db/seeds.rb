cfc41abeb93c803c7da6f03f8513fb92da48e9ec
edborden <borden.edward@gmail.com>
2015-04-10 16:24:25 -0400

    messagecontroller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,9 @@
-company = Company.create name: "TrapSmart",premium:true
-user = User.create name: "Ed Borden",email: "borden.edward@gmail.com",company_id:1,admin:true,password:"password"
-user = User.new name:"Tom",email:"tom@trapsmart.com",company_id:1,admin:true
-password = user.set_password
-user.save
-Mailer.new.welcome user,password
-user = User.new name:"Jim",email:"jim@trapsmart.com",company_id:1,admin:true
-password = user.set_password
-user.save
-Mailer.new.welcome user,password
-user = User.new name:"Shelby",email:"shelby@trapsmart.com",company_id:1,admin:true
-password = user.set_password
-user.save
-Mailer.new.welcome user,password
-user = User.new name:"Janet",email:"janet@trapsmart.com",company_id:1,admin:true
-password = user.set_password
-user.save
-Mailer.new.welcome user,password
-#25.times do
+Company.create name: "TrapSmart",premium:true
+User.create name: "Ed Borden",email: "borden.edward@gmail.com",company_id:1,admin:true,password:"password"
+User.create name:"Tom",email:"tom@trapsmart.com",company_id:1,admin:true,password:"password"
+User.create name:"Jim",email:"jim@trapsmart.com",company_id:1,admin:true,password:"password"
+User.create name:"Shelby",email:"shelby@trapsmart.com",company_id:1,admin:true,password:"password"
+User.create name:"Janet",email:"janet@trapsmart.com",company_id:1,admin:true,password:"password"
+=#25.times do
 #	trap = Fabricate(:trap,company_id:1)
 #end
