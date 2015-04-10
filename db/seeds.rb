5fd8f1fe8aa90b022af0c1a43bd06d644c6cbdec
edborden <borden.edward@gmail.com>
2015-04-10 16:28:13 -0400

    messagecontroller

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ User.create name:"Tom",email:"tom@trapsmart.com",company_id:1,admin:true,passwor
 User.create name:"Jim",email:"jim@trapsmart.com",company_id:1,admin:true,password:"password"
 User.create name:"Shelby",email:"shelby@trapsmart.com",company_id:1,admin:true,password:"password"
 User.create name:"Janet",email:"janet@trapsmart.com",company_id:1,admin:true,password:"password"
-=#25.times do
+#25.times do
 #	trap = Fabricate(:trap,company_id:1)
 #end
