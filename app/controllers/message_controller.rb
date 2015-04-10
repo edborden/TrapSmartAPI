56e6d20679520ea79558409c2a15ae48c7408320
edborden <borden.edward@gmail.com>
2015-04-10 16:14:04 -0400

    messagecontroller

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -4,7 +4,7 @@ class MessageController < ActionController::API
 	http_basic_authenticate_with name: "1faafdceeba45ad26f97a061b", password: "afbc67f939eea5b3f5753dba7c5fe"
 
 	def create
-		trap = Trap.find_by_imei params[:data][:device_name]
+		trap = Trap.find_by hardware_id: params[:data][:device_name]
 		if trap
 
 			## location
@@ -19,12 +19,12 @@ class MessageController < ActionController::API
 
 			## event
 			event = Event.new trap_id:trap.id
-			case params[:data][:event_code]
-			when "110"
+			case params[:data][:event_code].to_int
+			when 110
 				event.name = "Trap closed"
-			when "109"
+			when 109
 				event.name = "Sensor unit battery low"
-			when "89"
+			when 89
 				event.name = "Control unit battery low"
 			else
 				event.name = "Trap OK"
