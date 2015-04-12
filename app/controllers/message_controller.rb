707db4b982c1b81ae5e8e267311fc736a50e5f48
edborden <borden.edward@gmail.com>
2015-04-11 22:37:30 -0400

    filter code 91

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -4,45 +4,47 @@ class MessageController < ActionController::API
 	http_basic_authenticate_with name: "1faafdceeba45ad26f97a061b", password: "afbc67f939eea5b3f5753dba7c5fe"
 
 	def create
-		trap = Trap.find_by hardware_id: params[:data][:device_name]
-		if trap
+		event_code = params[:data][:event_code].to_i
+		unless event_code == 91
 
-			## location
-			location = trap.location
-			unless location
-				location = Location.create
-				trap.location = location
-			end
-			location.lat = params[:data][:gps_latitude]
-			location.lng = params[:data][:gps_longitude]
-			location.save
+			trap = Trap.find_by hardware_id: params[:data][:device_name]
+			if trap
 
-			## event
-			event = Event.new trap_id:trap.id
-			case params[:data][:event_code].to_i
-			when 110
-				event.name = "Trap closed"
-			when 109
-				event.name = "Sensor unit battery low"
-			when 89
-				event.name = "Control unit battery low"
-			when 108
-				event.name = "External power went above 10V"
-			when 30
-				event.name = "Powered on"
-			when 15,86
-				event.name = "Trap OK"
-			when 91
-				event.name = "Input 1 high (event 91)"
-			when 90
-				event.name = "Input 2 high (event 90)"
-			else
-				event.name = "Code #{params[:data][:event_code]}: #{params[:data][:event_type]}"
+				## location
+				location = trap.location
+				unless location
+					location = Location.create
+					trap.location = location
+				end
+				location.lat = params[:data][:gps_latitude]
+				location.lng = params[:data][:gps_longitude]
+				location.save
+
+				## event
+				event = Event.new trap_id:trap.id
+				case event_code
+				when 110
+					event.name = "Trap closed"
+				when 109
+					event.name = "Sensor unit battery low"
+				when 89
+					event.name = "Control unit battery low"
+				when 108
+					event.name = "External power went above 10V"
+				when 30
+					event.name = "Powered on"
+				when 15,86
+					event.name = "Trap OK"
+				else
+					event.name = "Code #{params[:data][:event_code]}: #{params[:data][:event_type]}"
+				end
+				event.send_notifications
+				event.set_trap_status
+
 			end
-			event.send_notifications
-			event.set_trap_status
 
 		end
+		
 		head :no_content
 	end
 
