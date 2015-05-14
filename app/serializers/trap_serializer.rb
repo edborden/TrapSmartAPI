c35e1299f6dd163227709f496323b8f69778845a
edborden <borden.edward@gmail.com>
2015-05-14 10:04:08 -0400

    Serializer fix

--- app/serializers/trap_serializer.rb
+++ app/serializers/trap_serializer.rb
@@ -1,6 +1,6 @@
 class TrapSerializer < ActiveModel::Serializer
 	attributes :id,:name,:hardware_id,:status,:status_updated
-	has_one :company, embed: :ids, include: false 
-	has_one :location, embed: :ids
-	has_many :notifications, embed: :ids
+	has_one :company, embed: :ids 
+	has_one :location, embed: :ids, include:true
+	has_many :notifications, embed: :ids, include:true
 end
