d2f26c2bb33ca46b0cb7ebd58c76937a8cbed1c2
edborden <borden.edward@gmail.com>
2015-05-27 12:13:29 -0400

    double render notifications in client

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,7 +1,7 @@
 class NotificationSerializer < ApplicationSerializer
 	attributes :mode,:target
-	has_one :company
-	has_one :trap
+	has_one :company, embed: :ids, include: false
+	has_one :trap, embed: :ids, include: false
 
 	def company
 		object.notifiable if object.notifiable.is_a? Company
